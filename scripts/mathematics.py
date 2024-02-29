29d21f8c7b35907feb6e12281a3c11b050f07643
FabianQuicken <f.quicken@sensorik.rwth-aachen.de>
2024-02-29 15:44:09 +0100

    euclidean distance function fixed

--- scripts/mathematics.py
+++ scripts/mathematics.py
@@ -5,6 +5,8 @@ Created on Thu Feb 29 08:13:00 2024
 @author: fabia
 """
 
+import numpy as np
+
 # calculate euclidean distance
 
 def euklidean_distance(x1, y1, x2, y2):
@@ -13,5 +15,7 @@ def euklidean_distance(x1, y1, x2, y2):
     (x1, y1) and (x2, y2) are the cartesian coordinates of the points.
     """
 
-    distance = (x2-x1)**2 + (y2-y1)**2
+    distance = np.sqrt((x2-x1)**2 + (y2-y1)**2)
     return distance
+
+print(euklidean_distance(2,2,1,1))
