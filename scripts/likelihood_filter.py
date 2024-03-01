55e593e7f4b6f7edad211a97cbbfc123fbfda190
FabianQuicken <f.quicken@sensorik.rwth-aachen.de>
2024-03-01 15:50:19 +0100

    bla

--- scripts/likelihood_filter.py
+++ scripts/likelihood_filter.py
@@ -9,14 +9,14 @@ Created on Thu Feb 29 08:20:54 2024
 
 import pandas as pd
 
-def likelihood_filtering(df, filter_val = 0.95):
+def likelihood_filtering(df, likelihood_row_name = "likelihood", filter_val = 0.95):
     """
     DeepLabCut provides a likelihood for the prediction of 
     each bodypart in each frame to be correct. Filtering predictions
     for the likelihood, reduces false predictions in the dataset.
     """
-    df_filtered = df[df["likelihood"] > 0.95]
-    df_removed_rows = df[df["likelihood"] < 0.95]
+    df_filtered = df[df[likelihood_row_name] > 0.95]
+    df_removed_rows = df[df[likelihood_row_name] < 0.95]
     print(f"The filter removed {len(df_removed_rows)} rows of a total of {len(df)} rows.")
     return df_filtered
     
